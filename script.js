9bbe72d3cb6a96fbdd39a6813f716b50e88b5212
Yulia <lavrinenkoll2003@gmail.com>
2022-11-05 23:05:38 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+/*при нажатии на кнопку сделать видимым блок с id=div_troposfera*/
+function showDiv(id_element) {
+    /*изменить класс элемента*/
+    document.getElementById(id_element).className = "div_"+id_element+"_visible";
+    /*переместить экран немного выше элемента*/
+    window.scrollTo(0, document.getElementById(id_element).offsetTop - 150);
+
+    document.getElementById(id_element+"button").style.opacity = 0;
+    document.getElementById(id_element+"button").style.display = "none";
+    document.getElementById(id_element+"text1").style.display = "none";
+    document.getElementById(id_element+"text2").style.display = "none";
+}
