2fb04be442a3a6b0cb09c4aa40b6ffdd47a1a6cd
Yulia <lavrinenkoll2003@gmail.com>
2022-11-06 19:03:55 +0200

    monkey flip

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function showDiv(id_element) {
     /*изменить класс элемента*/
     document.getElementById(id_element).className = "div_"+id_element+"_visible";
     /*переместить экран немного выше элемента*/
-    window.scrollTo(0, document.getElementById(id_element).offsetTop - 150);
+    window.scrollTo(0, document.getElementById(id_element).offsetTop + 200);
 
     document.getElementById(id_element+"button").style.opacity = 0;
     document.getElementById(id_element+"button").style.display = "none";
