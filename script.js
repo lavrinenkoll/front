621e0a91e51146e89ad9aec728382432ec427609
Yulia <lavrinenkoll2003@gmail.com>
2022-11-07 21:07:00 +0200

    hype button

--- script.js
+++ script.js
@@ -2,11 +2,40 @@
 function showDiv(id_element) {
     /*изменить класс элемента*/
     document.getElementById(id_element).className = "div_"+id_element+"_visible";
-    /*переместить экран немного выше элемента*/
-    window.scrollTo(0, document.getElementById(id_element).offsetTop + 200);
 
     document.getElementById(id_element+"button").style.opacity = 0;
     document.getElementById(id_element+"button").style.display = "none";
     document.getElementById(id_element+"text1").style.display = "none";
     document.getElementById(id_element+"text2").style.display = "none";
+    window.scrollTo(0, document.getElementById(id_element).offsetTop - 100);
+
+    /*подождать перед выполнением следующей функции*/
+    setTimeout(function() {
+        window.scrollTo(0, document.getElementById(id_element).offsetTop - 20);
+    }
+    , 1000);
+
+}
+
+function nextDiv(){
+        /*если текущий блок невидимый, то сделать его видимым*/
+    if (document.getElementById("troposfera").className == "div_troposfera") {
+        showDiv("troposfera");
+    }
+    else if (document.getElementById("stratosfera").className == "div_stratosfera") {
+        showDiv("stratosfera");
+    }
+    else if (document.getElementById("mesosfera").className == "div_mesosfera") {
+        showDiv("mesosfera");
+    }
+    else if (document.getElementById("termosfera").className == "div_termosfera") {
+        showDiv("termosfera");
+    }
+    else if (document.getElementById("exosfera").className == "div_exosfera") {
+        showDiv("exosfera");
+    }
+    else {
+        document.getElementById("finaltext1").style.display = "none";
+        window.scrollTo(0, document.getElementById("exosfera").offsetTop - 100);
+    }
 }
